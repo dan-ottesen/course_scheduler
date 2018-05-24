ec8380d08efc18b5cd06a7faf07bd48e6c70a865
Dan Ottesen <danott.dev@gmail.com>
2018-05-24 11:17:52 -0600

    Introduced Redux into app. Created courses reducer

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Course Library!</h1>
+        <h1>Course Library</h1>
         <CourseLibrary />
       </div>
     );
